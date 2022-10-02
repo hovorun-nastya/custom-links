b797e48847dac887e6c96ebccf089d92e2a7b3ef
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-10-02 18:06:21 +0200

    deploy

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { relayStylePagination } from '@apollo/client/utilities';
 
 const apolloClient = new ApolloClient({
-  uri: '/api/graphql',
+  uri: process.env.NODE_ENV === 'development'
+    ?'http://localhost:3000/api/graphql'
+    :'https://custom-links.vercel.app/api/graphql',
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
